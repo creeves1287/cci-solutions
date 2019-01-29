d2e46a7a5b180c274e25a95bed51dba5efff2012
creeves1287 <45729008+creeves1287@users.noreply.github.com>
2019-01-28 17:46:52 -0800

    added BinaryToString converter

---
+++ Ch5 - Bit Manipulation/BinaryToString/BinaryConverterTests/BinaryConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BinaryToString;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryConverterTests
+{
+    [TestClass]
+    public class BinaryConverterTests
+    {
+        [TestMethod]
+        public void MyBinaryConverterTest()
+        {
+            IBinaryConverter binaryConverter = new MyBinaryConverter();
+            RunTests(binaryConverter);
+        }
+
+        private void RunTests(IBinaryConverter binaryConverter)
+        {
+            TestBinaryConversion(binaryConverter);
+        }
+
+        private void TestBinaryConversion(IBinaryConverter binaryConverter)
+        {
+            int x = 54;
+            string expected = "110110";
+            string result = binaryConverter.BinaryToString(x);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Ch5 - Bit Manipulation/BinaryToString/BinaryConverterTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryConverterTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BinaryConverterTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ff22dc3-54b9-4668-b9f8-5a90226ccc3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
