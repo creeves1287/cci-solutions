75a30f46f678e2dbdbb49c97d45ab28029bbe261
creeves1287 <45729008+creeves1287@users.noreply.github.com>
2019-02-27 17:37:09 -0800

    updated next number finder with working arithmetic solution

--- Ch5 - Bit Manipulation/NextNumberFinder/NextNumberFinder/TextbookNextNumberFinderWithArithmetic.cs
+++ Ch5 - Bit Manipulation/NextNumberFinder/NextNumberFinder/TextbookNextNumberFinderWithArithmetic.cs
@@ -17,6 +17,7 @@ namespace NextNumberFinder
         }
 
         //1000111000 => 1001000000 => 1001000100 => 1001000011
+        //11011101111 => 11011110000 => 11011110111
         private int GetNextNumber(int n)
         {
             int c0 = 0,
@@ -37,12 +38,15 @@ namespace NextNumberFinder
             }
 
             int p = c0 + c1;
-            int result = n + (1 << (c0 + 1)) + (1 << c1) - 1;
+            int result = n + (1 << c0) + (1 << (c1 - 1)) - 1;
+            //Simplified: int result = n + (1 << (p - 1));
             return result;
         }
 
         //1000111000 => 1000110100
         //1011001111 => 1010110111 => 1010111000 =>  1010111100
+        //11011101111 => 110111011111 => 11011110111
+
         private int GetPreviousNumber(int n)
         {
             int c0 = 0,
@@ -63,7 +67,8 @@ namespace NextNumberFinder
             }
 
             int p = c0 + c1;
-            int result = n - (1 << p) + (1 << (c0 + 1)) + 1;
+            int result = n - (1 << c1) - (1 << (c0 - 1)) + 1;
+            //Simplified: int result = n - (1 << (p - 1));
             return result;
         }
     }
