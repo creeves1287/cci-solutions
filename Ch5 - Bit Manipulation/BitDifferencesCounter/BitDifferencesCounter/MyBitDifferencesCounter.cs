cdd31b6e5fdc0c7fe5f58e174d58ecfc9a2638c7
creeves1287 <45729008+creeves1287@users.noreply.github.com>
2019-02-28 18:26:38 -0800

    added bit differences counter

---
+++ Ch5 - Bit Manipulation/BitDifferencesCounter/BitDifferencesCounter/MyBitDifferencesCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitDifferencesCounting
+{
+    public class MyBitDifferencesCounter : IBitDiffernecesCounter
+    {
+        public int CountBitDifferences(int n, int m)
+        {
+            if (n == m) return 0;
+
+            int counter = 0;
+            while (m != 0 || n != 0)
+            {
+                if ((n & 1) != (m & 1))
+                    counter++;
+
+                n >>= 1;
+                m >>= 1;
+            }
+
+            return counter;
+        }
+    }
+}
