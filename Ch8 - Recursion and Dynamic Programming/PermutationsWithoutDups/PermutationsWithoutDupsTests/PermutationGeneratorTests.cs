c462477f776516975754e10194fb66fe88ede1da
creeves1287 <45729008+creeves1287@users.noreply.github.com>
2019-06-17 18:17:01 -0700

    added Permutations without Dups project

---
+++ Ch8 - Recursion and Dynamic Programming/PermutationsWithoutDups/PermutationsWithoutDupsTests/PermutationGeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PermutationsWithoutDups;
+
+namespace PermutationsWithoutDupsTests
+{
+    [TestClass]
+    public class PermutationGeneratorTests
+    {
+        [TestMethod]
+        public void MyPermutationGeneratorTest()
+        {
+            IPermutationGenerator permutationGenerator = new MyPermutationGenerator();
+            RunTests(permutationGenerator);
+        }
+
+        [TestMethod]
+        public void TextbookPermutationGeneratorTest()
+        {
+            IPermutationGenerator permutationGenerator = new TextbookPermutationsGenerator();
+            RunTests(permutationGenerator);
+        }
+
+        private void RunTests(IPermutationGenerator permutationGenerator)
+        {
+            string s = "abcde";
+            List<string> results = permutationGenerator.GeneratePermutations(s);
+            PrintResults(results);
+        }
+
+        private void PrintResults(List<string> results)
+        {
+            for (int i = 0; i < results.Count; i++)
+            {
+                string result = results[i];
+                Trace.WriteLine(result);
+            }
+            Trace.WriteLine($"Total Permutations: {results.Count}");
+        }
+    }
+}
